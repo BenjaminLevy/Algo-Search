3b4243106fb876c724449fda407dd5b74192f403
Benjamin <BnLvy1@gmail.com>
2024-03-08 11:24:36 -0800

    updates to beej crawler

--- algo_search_project/spiders/beej_guide_to_c.py
+++ algo_search_project/spiders/beej_guide_to_c.py
@@ -4,21 +4,30 @@ import scrapy
 import json
 
 class BeejGuideToCSpider(scrapy.Spider):
+
+    custom_settings = {
+        "DEPTH_LIMIT": 0
+    }
     name = "beej_guide_to_c"
     allowed_domains = ["beej.us"] # change to 
-    start_urls = ["https://beej.us/guide/bgc/html/split/foreword.html"]
+    start_urls = [
+            "https://beej.us/guide/bgc/html/split/foreword.html",
+            "https://beej.us/guide/bgnet/html/split/intro.html",
+            "https://beej.us/guide/bgnet0/html/split/foreword.html",
+            "https://beej.us/guide/bgipc/html/split/intro.html",
+            ]
     # file:///home/benjamin/coding/algo-search/algo_search_project/site.html
     # https://beej.us/guide/bgc/html/split/foreword.html
     def parse(self, response):
-        page_title_formatted = response.css("h1::text").get()
-        page_title_snake_case = response.css("h1::attr(id)").get()
+        chapter_title_formatted = response.css("h1::text").get()
         parsed_text_array = (response.xpath("//body//text()").getall())
         parsed_text_trimmed = parsed_text_array[6:-5]
         final_text = [item for item in parsed_text_trimmed if item != '\n']
         final_text = ' '.join(final_text)
 
         data_obj = {
-                'page_title': page_title_formatted,
+                'title_element': response.css('title::text').get(),
+                'chapter_title': chapter_title_formatted,
                 'url': response.url,
                 'body': final_text
                 }
