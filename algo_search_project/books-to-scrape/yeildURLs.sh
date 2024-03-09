ad79023ae299f770fe869a4dc4b77835e14e96f0
Benjamin <BnLvy1@gmail.com>
2024-03-08 16:45:26 -0800

    wip

--- algo_search_project/books-to-scrape/yeildURLs.sh
+++ algo_search_project/books-to-scrape/yeildURLs.sh
@@ -8,11 +8,15 @@ end=$((${1} + 10))
 main() {
 	case $1 in
 		+([0-9]))
-			urlArr=`head --lines=${end} book-urls.txt | tail`
-			echo "current URLs:"
-			echo $urlArr
-			# Redirect stderr to circumvent Chrome error message 
-			for item in "${urlArr[@]}"; do google-chrome $item 2>/dev/null; done
+			while true; do
+				urlArr=`head --lines=${end} good-urls.txt | tail`
+				echo "current URLs:"
+				echo $urlArr
+				for item in "${urlArr[@]}"; do processLink item 
+				done
+				end=$((${end} + 10))
+				read -p "press any key to open 10 more" -n1 -s
+			done
 			;;
 		*)
 			echo "Argument not recognized."
@@ -22,9 +26,16 @@ main() {
 	esac
 exit 0
 }
+
+function processLink(){
+	echo $1
+	# Redirect stderr to circumvent Chrome error message 
+	# google-chrome $1 2>/dev/null
+}
 function displayHelp()
 {
 	echo "Usage:"
 	echo "yeildURLs {line number to start on}"
 }
+
 main "$@"; 
